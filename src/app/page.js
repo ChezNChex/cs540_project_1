721d799442658e8d4d39252ff6c5995d4b65c303
Jimmy Nguyen <jimmy123_nguyen@icloud.com>
2025-02-27 18:47:22 -0600

    a

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import WhatButton from './what-button';
+import OkButton from './ok-button';
 import styles from "./chartstyle.module.css";
 import chart from "./process.js";
 function Header({ title }) {
@@ -8,20 +8,33 @@ function Header({ title }) {
 
 
 export default function HomePage() {
-    const names = ['holy','moly','guacamole'];
-    
+    const processes = ['First In, First Out','Shortest Job First','Shortest Time-To-Completion First','Round Robin','Multi-Level Feedback Queue'];
     return (
-        <div>
-            <Header title="Crazy"/>
-            <div className={styles.chartsize}><canvas id="FIFO"></canvas></div>
-            <script type="module" src="process.js"></script>
+            <div>
+            <Header title="CPU Scheduling Algorithms"/>
+            <div>
+            <form id="userInput">
+            <label htmlFor="number">Enter # of processes: </label>
+            <input type="int" id="number" name="number"></input>
+            <label htmlFor="timeQuantum"> Enter Time Quantum (for RR): </label>
+            <input type="int" id="timeQuantum" name="timeQuantum"></input>
+            <OkButton />
+            </form>
+            </div>
+            <script>
+                const number = document.getElementById('number').value;
+                const timeQuantum = document.getElementById('timeQuantum').value;
+                </script>
             <ul>
-                {names.map((name) => (
-                    <li key={name}>{names}</li>
-                ))}
+            {processes.map((process) => (
+                                         <li key={process}><h3>{process}</h3>
+                                         <canvas id="FIFO"></canvas>
+                                         </li>
+                                         ))}
             </ul>
-            <WhatButton />
-        </div>
             
-    );
+            </div>
+            
+            );
 }
+
